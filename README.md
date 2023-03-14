7caeabc174361ac5994cb34a63280abd0f9d95ed
DaniloAlvesz <79539124+DaniloAlvesz@users.noreply.github.com>
2023-03-14 08:54:05 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Quitanda-do-PD
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-q7uqtl-j1l9dz)
---
+++ src/app/products.ts
@@ -0,0 +1,46 @@
+export interface Product {
+  id: number;
+  name: string;
+  image: ImageData;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Manga',
+    price: 4.51,
+    description: 'A Manga é verde',
+  },
+  {
+    id: 2,
+    name: 'Jaca',
+    price: 10.95,
+    description: 'A Jaca é amarela',
+  },
+  {
+    id: 3,
+    name: 'Laranja',
+    price: 8.12,
+    description: 'A laranja é doce',
+  },
+  {
+    id: 3,
+    name: 'Batata',
+    price: 2.25,
+    description: 'A batata é frita',
+  },
+  {
+    id: 3,
+    name: 'Macaxeira',
+    price: 9.99,
+    description: 'A batata é frita',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
